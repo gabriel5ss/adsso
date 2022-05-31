04fc29cdeac3f4a347283e2aa95da88a80c15911
gabriel5ss <gabriel.psn2350@gmail.com>
2022-05-31 11:33:37 -0300

    removendo o dd

--- app/Providers/SAML2ServiceProvider.php
+++ app/Providers/SAML2ServiceProvider.php
@@ -77,7 +77,7 @@ class SAML2ServiceProvider extends ServiceProvider
                 'assertion' => $user->getRawSamlAssertion()
             ];
             
-            dd($userData);
+            // dd($userData);
 
             $laravelUser = 2; //find user by ID or attribute
             //if it does not exist create it and go on  or show an error message
